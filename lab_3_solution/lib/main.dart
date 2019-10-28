33b20bd0bc7dbe61b333e6065e8ee54c94a41c1e
Robbin <robbinl@telusplanet.net>
2019-10-28 14:31:14 -0600

    a

--- lab_3_solution/lib/main.dart
+++ lab_3_solution/lib/main.dart
@@ -1,4 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:lab3/widget/StoreHomePage.dart';
 
-void main() => runApp(MyAppLab3());
+class Person {
+  String firstName;
+  String lastName;
+
+  Person(this.firstName, this.lastName);
+}
+
+void main() {
+  List<Person> people = new List<Person>();
+  people.add(new Person("Joe", "Smithers"));
+  people.add(new Person("Patrick", "Thomas"));
+  for (var n in people) {
+    print('Hey ${n.firstName} ${n.lastName}');
+  }
+  var mappedNames =
+      people.map((n) => 'Mr. ${n.firstName} ${n.lastName}').toList();
+  for (var x in mappedNames) {
+    print(x);
+  }
+
+  runApp(MyAppLab3());
+}
+
+//void main() => runApp(MyAppLab3());
