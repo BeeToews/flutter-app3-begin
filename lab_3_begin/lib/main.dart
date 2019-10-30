ca97b245da4a56c08ddbad41d84883384a084cb9
Robbin Law <robbinl@telusplanet.net>
2019-10-30 12:59:55 -0600

    a

--- lab_3_begin/lib/main.dart
+++ lab_3_begin/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:coffee_store_app/widget/StoreHomePage.dart';
-import 'package:coffee_store_app/widget/ac_tabbed_page_demo_1.dart';
-import 'package:coffee_store_app/widget/ae_tabbed_page_demo_2.dart';
-import 'package:coffee_store_app/widget/ag_tabbed_page_demo_3.dart';
-import 'package:coffee_store_app/widget/aj_scoped_model_demo_1.dart';
-import 'package:coffee_store_app/widget/am_tabbed_page_scoped_model_demo.dart';
+import 'package:coffee_store_app/demos/ac_tabbed_page_demo_1.dart';
+import 'package:coffee_store_app/demos/ae_tabbed_page_demo_2.dart';
+import 'package:coffee_store_app/demos/ag_tabbed_page_demo_3.dart';
+import 'package:coffee_store_app/demos/aj_scoped_model_demo_1.dart';
+import 'package:coffee_store_app/demos/am_tabbed_page_scoped_model_demo.dart';
 
 //void main() => runApp(MyTabbedApp1());
 //void main() => runApp(MyTabbedApp2());
