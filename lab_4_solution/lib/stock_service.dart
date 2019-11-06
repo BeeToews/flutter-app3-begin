54270e2f822701d422bc1c842f74eefca6ca4016
Robbin <robbinl@telusplanet.net>
2019-11-06 12:20:14 -0700

    a

--- lab_4_solution/lib/stock_service.dart
+++ lab_4_solution/lib/stock_service.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart' as http;
 import 'dart:async';
+import 'dart:convert';
 
 class StockService {
   Future<double> getPrice(String symbol) async {
@@ -8,8 +9,18 @@ class StockService {
     http.Response response = await http.get(url);
     print(response.statusCode);
     print(response.body);
-    //double price = double.tryParse(response.body);
-    //print(price);
+    if (response.statusCode == 200) {
+      var data = response.body;
+      print(data);
+      var symbol = jsonDecode(response.body)['symbol'];
+      print(symbol);
+      var companyName = jsonDecode(response.body)['companyName'];
+      print(companyName);
+      var price = jsonDecode(response.body)['latestPrice'];
+      print(price);
+    } else {
+      print(response.statusCode);
+    }
     return 3.00; //price;
   }
 }
