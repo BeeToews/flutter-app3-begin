779902cfd768176ed693f8bc88d2f577e87abb0a
robbinl <robbinl@telusplanet.net>
2020-10-14 13:55:56 -0600

    a

--- app3_begin/lib/main.dart
+++ app3_begin/lib/main.dart
@@ -8,7 +8,7 @@ import 'package:coffee_store_app/demos/am_tabbed_page_scoped_model_demo.dart';
 
 //void main() => runApp(MyTabbedApp1());
 //void main() => runApp(MyTabbedApp2());
-void main() => runApp(MyTabbedApp3());
-//void main() => runApp(MyScopeModelApp1(model: CounterModel()));
+//void main() => runApp(MyTabbedApp3());
+void main() => runApp(MyScopeModelApp1(model: CounterModel()));
 //void main() => runApp(MyTabbedScopedModelApp1());
 //void main() => runApp(MyApp3());
