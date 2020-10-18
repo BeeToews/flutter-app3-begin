d38b74c2a8c1a248a798faad09fbec6795192632
robbinl <robbinl@telusplanet.net>
2020-10-18 13:29:46 -0600

    a

--- app3_begin/ios/Flutter/flutter_export_environment.sh
+++ app3_begin/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\FlutterDev\FlutterSDK\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\FlutterDev\MyAppRepos\flutter-dev\lab_3_begin"
+export "FLUTTER_APPLICATION_PATH=C:\FlutterDev\MyAppRepos\flutter-app3-begin\app3_begin"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
--- lab_4_begin/lib/stock.dart
+++
@@ -1,8 +0,0 @@
-
-class Stock {
-  String symbol;
-  String name;
-  double price;
-
-  Stock(this.symbol, this.name, this.price);
-}
