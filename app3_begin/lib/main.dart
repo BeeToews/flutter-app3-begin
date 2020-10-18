6a7ac48783ba75deec7b4d3ebdccee235605b32f
robbinl <robbinl@telusplanet.net>
2020-10-18 13:15:31 -0600

    a

--- app3_begin/lib/main.dart
+++ app3_begin/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:coffee_store_app/widget/StoreHomePage.dart';
 import 'package:coffee_store_app/demos/ac_tabbed_page_demo_1.dart';
 import 'package:coffee_store_app/demos/ae_tabbed_page_demo_2.dart';
 import 'package:coffee_store_app/demos/ag_tabbed_page_demo_3.dart';
-
+import 'package:coffee_store_app/demos/aj_scoped_model_demo_1.dart';
 import 'package:coffee_store_app/demos/ak_scoped_model_demo_2.dart';
 import 'package:coffee_store_app/demos/am_tabbed_page_scoped_model_demo.dart';
 
